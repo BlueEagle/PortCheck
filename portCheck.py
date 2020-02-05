3f1b77ded1fd8c6ad0b62afea5ada3458a41974c
Blueeagle <Blueeagle@balloucollin@gmail.com>
2020-02-05 12:08:43 -0700

    It refreshes at intervals now.

--- portCheck.py
+++ portCheck.py
@@ -3,15 +3,13 @@ import socket
 import subprocess
 import sys
 import pyfiglet
+import time
 from datetime import datetime
-
-
-# clear the screen
-subprocess.call('clear', shell=True)
 
 # variables and informing the user
 firstPort = 1
 lastPort = 65535
+waitTime = 3
 
 # Handling multiple or no arguments
 if len(sys.argv)>1:
@@ -19,41 +17,59 @@ if len(sys.argv)>1:
 else:
     remoteServer = "localhost"
 
+def main():
+    # clear the screen
+    subprocess.call('clear', shell=True)
 
-remoteServerIP = socket.gethostbyname(remoteServer)
-print(pyfiglet.figlet_format("PortCheck"))
-print("Scanning ports on machine: ", remoteServer, "\t(IP: ", remoteServerIP, ")\n\n")
 
-# keeping time
-startScanTime = datetime.now()
+    remoteServerIP = socket.gethostbyname(remoteServer)
+    print(pyfiglet.figlet_format("PortCheck"))
+    print("Scanning ports on machine: ", remoteServer, "\t(IP: ", remoteServerIP, ")\n\n")
 
-try:
-    print("Port:\t\tService:")
-    for port in range(firstPort, lastPort):
-        sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-        sock.settimeout(0.5)
-        result = sock.connect_ex((remoteServerIP, port))
-        if result == 0:
-            print(port, "\t\tN/A")
-        sock.close()
+    # keeping time
+    startScanTime = datetime.now()
 
-except KeyboardInterrupt:
-    print("\n\nExcuse me! I wasn't finished!")
-    sys.exit()
+    try:
+        print("Port:\t\tService:")
+        for port in range(firstPort, lastPort):
+            sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+            sock.settimeout(0.5)
+            result = sock.connect_ex((remoteServerIP, port))
+            if result == 0:
+                print(port, "\t\tN/A")
+            sock.close()
 
-except socket.gaierror:
-    print("\n\nTarget hostname could not resolve, try something else.")
-    sys.exit()
+    except KeyboardInterrupt:
+        print("\n\nExcuse me! I wasn't finished!")
+        sys.exit()
 
-except socket.error:
-    print ("\n\nSomething went wrong, generic error text here...")
-    sys.exit()
+    except socket.gaierror:
+        print("\n\nTarget hostname could not resolve, try something else.")
+        sys.exit()
 
-# still keeping time
-endScanTime = datetime.now()
+    except socket.error:
+        print ("\n\nSomething went wrong, generic error text here...")
+        sys.exit()
 
-# let me tell you how slow I am
-elapsedTime = endScanTime - startScanTime
+    # still keeping time
+    endScanTime = datetime.now()
 
-# wrapping things up, for now...
-print("\n\nTotal scan time: ", elapsedTime)
+    # let me tell you how slow I am
+    elapsedTime = endScanTime - startScanTime
+
+    # wrapping things up, for now...
+    print("\n\nTotal scan time: ", elapsedTime)
+
+while True:
+    main()
+
+    try:
+        time.sleep(waitTime)
+
+    except KeyboardInterrupt:
+        print("\n\nExcuse me! I was about to refresh!")
+        sys.exit()
+
+    except socket.error:
+        print ("\n\nSomething went wrong, generic error text here...")
+        sys.exit()
